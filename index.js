ee0334df11a6dd14c3cdeeb0be6c62379b33b236
uzair <uzairhakeem71@gmail.com>
2024-04-18 15:44:18 +0500

    adding files

---
+++ index.js
@@ -0,0 +1,19 @@
+//Import mongoose database
+// var cors = require('cors') //Import cores using express
+const connecttoMongoose = require('./db/db.js')
+connecttoMongoose();
+//import express
+
+const express = require('express')
+const app = express()
+const bodyParser=require('body-parser');
+// app.use(cors()) //use cores to fetch the data in the browser
+const port = 5000
+//req to server using json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));//Routes(paths)
+app.use('/auth',require('./Routes/User/Auth/signup.js'))
+app.use('/auth',require('./Routes/User/Auth/login.js'))
+app.listen(port, () => {
+  console.log(`App is listening on port http://localhost:${port}`)
+})
