415afc54b9958030a90b7cf5556f27bc1abf7a9f
UZAIR HAKEEM <151864936+Uzair73@users.noreply.github.com>
2024-07-20 16:25:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const bodyParser=require('body-parser');
 
 // Cors Policy
 const corsOptions = {
-  origin: ['https://clinic-managment-frontend.vercel.app'],
+  origin: ['*'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow cookies to be sent
