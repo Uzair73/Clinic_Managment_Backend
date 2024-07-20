295f3af09edd70340c04968fb0a23dce71e87f36
UZAIR HAKEEM <151864936+Uzair73@users.noreply.github.com>
2024-07-20 13:39:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const bodyParser=require('body-parser');
 
 // Cors Policy
 const corsOptions = {
-  origin: ['https://example.com', 'https://another-domain.com'],
+  origin: ['http://localhost:5000'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow cookies to be sent
